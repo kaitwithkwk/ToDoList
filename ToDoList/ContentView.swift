01d3906d8a11e16a7e06b87205ce2c432238d355
Scholar Name <scholar@kodewithklossy.com>
2025-04-14 13:14:30 -0500

    Add animation

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -18,7 +18,9 @@ struct ContentView: View {
                     .fontWeight(.black)
                 Spacer()
                 Button {
-                    showNewTask = true
+                    withAnimation {
+                        showNewTask = true
+                    }
                 } label: {
                     Text("+")
                         .font(.title)
